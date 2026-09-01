1b9f45de7c55905ae5e8cb7a6a3ed7cc3dbbc146
docdray <soeren.dressler@gmail.com>
2026-09-01 16:56:09 +0200

    kafka ergänzt

--- code-with-quarkus-kotlin1/src/main/kotlin/org/acme/ExampleResource.kt
+++ code-with-quarkus-kotlin1/src/main/kotlin/org/acme/ExampleResource.kt
@@ -3,12 +3,21 @@ package org.acme
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
+import org.eclipse.microprofile.reactive.messaging.Channel
+import org.eclipse.microprofile.reactive.messaging.Emitter
 import jakarta.ws.rs.core.MediaType
+import java.time.Instant
 
 @Path("/hello")
 class ExampleResource {
 
+    @Channel("kafkatest-out")
+    lateinit var emitter: Emitter<String>
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello from Quarkus REST"
+    fun hello(): String {
+        emitter.send("Hello aufgerufen um ${Instant.now()}")
+        return "Hello from Quarkus REST"
+    }
 }
